f0eae94b11766eb886c52dba661c00166ca8d792
Jesus <jesusmontoya1992@yahoo.com>
2024-07-18 00:17:02 -0500

    added initial state variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [toDoList, setToDoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
   return (
     <div className="App">
-      
+      <div className='addTask'>
+        <input></input>
+        <button>Add Task</button>
+      </div>
+      <div className='list'>
+
+      </div>
     </div>
   );
 }
