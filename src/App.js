95f10104a8d11ef90a1d408ee315500576a6c4ca
Jesus <jesusmontoya1992@yahoo.com>
2024-07-18 15:54:59 -0500

    added delete task function

--- src/App.js
+++ src/App.js
@@ -6,13 +6,47 @@ function App() {
   const [toDoList, setToDoList] = useState([]);
   const [newTask, setNewTask] = useState("");
 
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const task = {
+      id: toDoList.length === 0 ? 1 : toDoList[toDoList.length - 1].id + 1,
+      taskName: newTask,
+    };
+    // const newToDoList = [...toDoList, newTask];
+    setToDoList([...toDoList, task]);
+  };
+
+  const deleteTask = (id) => {
+    const newToDoList = toDoList.filter((task) => {
+      return task.id !== id;
+      // if (task === taskName) {
+      //   return false;
+      // } else {
+      //   return true;
+      // }
+    });
+
+    setToDoList(newToDoList);
+  };
+
   return (
     <div className="App">
       <div className='addTask'>
-        <input></input>
-        <button>Add Task</button>
+        <input onChange={handleChange}></input>
+        <button onClick={addTask}>Add Task</button>
       </div>
       <div className='list'>
+        {toDoList.map((task) => {
+          return (
+            <div>
+              <h1>{task.taskName}</h1>
+              <button onClick={() => deleteTask(task.id)}>X</button>
+            </div>
+          );
+        })}
 
       </div>
     </div>
